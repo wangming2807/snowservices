f8090bde292c393179c6f106b51df432d121d27b
mrsnowflake <mrsnowflake@a7183584-8db0-11dd-aa02-45bbecb07bf0>
2008-10-01 14:47:13 +0000

    Proof of concept character recognizer

--- SnowGesture/src/eu/MrSnowflake/android/snowgestures/R.java
+++ SnowGesture/src/eu/MrSnowflake/android/snowgestures/R.java
@@ -21,7 +21,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int char_recognizer_name=0x7f040002;
         public static final int gesture=0x7f040000;
-        public static final int info=0x7f040002;
+        public static final int info=0x7f040003;
     }
 }
